9ca0fec68f7adf4b89ed91773c81c34f9063bb89
Montasir Mahmud <mmcse19@gmail.com>
2022-07-02 00:41:13 +0600

    Server restart

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-router.post("/registration_old", userController.create_new_user);
+router.post("/registration_old", userController.auth_user);
 router.post("/registration", userController.createUser);
 router.post("/login", userController.authUser);
 
